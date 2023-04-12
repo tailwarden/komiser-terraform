f7d6ef18b7b92599c0f91174fcaefaf094466c3d
Mohamed Labouardy <mohamed@labouardy.com>
2023-04-12 17:09:48 +0200

    feat: fetch ami with terraform

--- ec2.tf
+++ ec2.tf
@@ -1,23 +1,37 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
 resource "aws_instance" "komiser" {
-  ami           = "ami-0fa03365cde71e0ab"
-  instance_type = var.instance_type
-  key_name = var.key
+  ami                         = data.amazon_linux.id
+  instance_type               = var.instance_type
+  key_name                    = var.key
   associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.komiser_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.komiser_instance_profile.name
+  vpc_security_group_ids      = [aws_security_group.komiser_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.komiser_instance_profile.name
   //user_data = file("scripts/install.sh")
 
   tags = {
-    Name = "komiser"
+    Name  = "komiser"
     Owner = var.owner
   }
 
   connection {
-    type = "ssh"
-    user = "ec2-user"
+    type        = "ssh"
+    user        = "ec2-user"
     private_key = file(var.private_key_path)
-    host = self.public_ip
-  } 
+    host        = self.public_ip
+  }
 
   provisioner "file" {
     source      = "scripts/config.toml"
@@ -44,9 +58,9 @@ resource "aws_security_group" "komiser_sg" {
   description = "Allow traffic on port 3000 and enable SSH"
 
   ingress {
-    from_port       = "22"
-    to_port         = "22"
-    protocol        = "tcp"
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -65,7 +79,7 @@ resource "aws_security_group" "komiser_sg" {
   }
 
   tags = {
-    Name   = "komiser_sg"
+    Name  = "komiser_sg"
     Owner = var.owner
   }
 }
